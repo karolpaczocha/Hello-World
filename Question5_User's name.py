f424535be9129d5e7d8fbe178c6c114f4c421039
karolpaczocha <32064432+karolpaczocha@users.noreply.github.com>
2017-10-02 14:04:09 +0100

    Add files via upload

---
+++ Question5_User's name.py
@@ -0,0 +1,10 @@
+name = input('''Hello user, whats your name?
+             ''')# This command takes the name of the
+                 # user which is printed out later
+print(name, "What a nice name")
+
+Feels = input('''How is it going?
+             ''')# Now the program is asking the user how
+                 # how he/she is feeling
+print("Thats great!")
+
